b5369f615bafbacc4ea5781754fcc1086facd023
Alexandre Delacre <delacre.alex@gmail.com>
2023-12-18 16:55:47 +0100

    creation requete pour recupération des events + affichage des marqueurs + affichage list

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,12 +2,13 @@ import {
   StyleSheet,
   Text,
   TextInput,
-  TouchableOpacity, 
+  TouchableOpacity,
   View,
   Modal,
   Image,
   ScrollView,
   TouchableWithoutFeedback,
+  Platform
 } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
@@ -30,6 +31,8 @@ export default function HomeScreen({ navigation }) {
   const [isCheckedPromo, setCheckedPromo] = useState('');
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
   const [selectedStartDate, setSelectedStartDate] = useState(null);
+  const [date, setDate] = useState(new Date())
+  const [eventsData, setEventsData] = useState(null)
 
   const [fontsLoaded] = useFonts({
     'Quicksand-Bold': require('../assets/fonts/Quicksand-Bold.ttf'),
@@ -39,223 +42,200 @@ export default function HomeScreen({ navigation }) {
 
   const showDatePicker = () => {
     setDatePickerVisibility(true);
-};
-const hideDatePicker = () => {
+  };
+  const hideDatePicker = () => {
     setDatePickerVisibility(false);
-};
-const handleConfirm = (date) => {
+  };
+  const handleConfirm = (date) => {
     console.warn("Date sélectionnée: ", date);
     setSelectedStartDate(date);
     hideDatePicker();
-};
+  };
 
-  
+
   useEffect(() => {
     (async () => {
       const { status } = await Location.requestForegroundPermissionsAsync();
       if (status === 'granted') {
         Location.watchPositionAsync({ distanceInterval: 10 },
           (location) => {
-            setCurrentPosition(location.coords);
+            fetch('http://10.1.1.249:3000/events/display', {
+              method: 'POST',
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({ latitude: location.coords.latitude, longitude: location.coords.longitude, date: date }),
+            })
+              .then(response => response.json())
+              .then(data => {
+                setEventsData(data.data)
+                setCurrentPosition(location.coords);
+              })
           });
-        }
-      })();
+      }
+    })();
   }, []);
 
-// Fonts import
+  let event
+  let eventList
+  if (eventsData) {
+    event = eventsData.map((data, i) => {
+      const latitude = data.etablissement.localisation.coordinates[1]
+      const longitude = data.etablissement.localisation.coordinates[0]
+      return <Marker key={i} coordinate={{ latitude, longitude }} title={data.etablissement.name} pinColor='#8440B4' />
+    })
+    eventList = eventsData.map((data, i) => {
+      return (
+        <View style={styles.etablishmentCard}>
+          <Image style={styles.image} source={{uri: data.etablissement.photos[0]}} />
+          <TouchableOpacity onPress={() => {
+            setEtablishmentsModalVisible(false);
+            navigation.navigate('Description')
+          }}>
+            <View style={styles.etablishmentInfo}>
+              <Text style={styles.etablishmentName}>{data.etablissement.name}</Text>
+              <Text style={styles.etablishmentDistance}>à XXX mètres</Text>
+              <Text style={styles.etablishmentNote}>Note Google: 3,9/5</Text>
+              <Text style={styles.etablishmentAdress}>{data.etablissement.adresse}</Text>
+              <Text style={styles.etablishmentPhone}>{data.etablissement.telephone}</Text>
+              <Text style={styles.etablishmentEvent}>{data.title}</Text>
+              <Text style={styles.etablishmentEvent}>{data.description}</Text>
+            </View>
+          </TouchableOpacity>
+          <View style={styles.etablishmentFavorite}>
+            <FontAwesome name='star' color={'#D7D7E5'} size={25} />
+          </View>
+        </View>
+      )
+    })
+  }
+
+  console.log(eventList)
+
+  // Fonts import
   if (!fontsLoaded) {
     return null
   }
 
   return (
-  <View style={styles.container}>
-{/* Filter view */}
-    <View style={styles.filterContainer}>
-      <View style={styles.filter}>
-        <TextInput 
-          placeholder={'Adresse, ville, métro...'}
-          style={styles.filterPlace}
-        />
-        <View style={styles.filterBottom}>
-          <TextInput 
-                placeholder='Quand ?' 
-                onFocus={showDatePicker}
-                value={selectedStartDate ? format(selectedStartDate, 'dd/MM/yy HH:mm') : ''}
-                style={styles.filterWhen} 
-                />
-              <DateTimePickerModal
-                isVisible={isDatePickerVisible}
-                mode="datetime"
-                onConfirm={handleConfirm}
-                onCancel={() => { hideDatePicker() }}
-                locale="fr"
-                cancelTextIOS="Annuler" // A voir pour Android
-                confirmTextIOS="Confirmer" // A voir pour Android
-                />
-          <TouchableOpacity
-            style={styles.buttonFilter}
-            activeOpacity={0.8}
-            onPress={() => setIsFilterModalVisible(!isFilterModalVisible)}
-          >
-            <Text style={styles.textButtonFilter}>+ de filtres</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </View>
-{/* Filter modal*/}
-    <Modal
-      animationType='fade'
-      transparent={true}
-      visible={isFilterModalVisible}
-      onRequestClose={() => setIsFilterModalVisible(false)}
-    >
-      <TouchableWithoutFeedback onPress={() => setIsFilterModalVisible(false)}>
-        <View style={styles.modal}>
-          <View style={styles.modalView}>
-            <View style={styles.checkboxContainer}>
-              <Text style={styles.checkboxText}>Bar :</Text>
-              <Checkbox
-                style={styles.checkbox}
-                value={isCheckedBar}
-                onValueChange={() => setCheckedBar(isCheckedBar === '' ? 'Bar' : '')}
-                color={isCheckedBar !== '' ? '#E6DCEC' : undefined}
-              />
-              <Text style={styles.checkboxText}>Restaurant :</Text>
-              <Checkbox
-                style={styles.checkbox}
-                value={isCheckedRestaurant}
-                onValueChange={() => setCheckedRestaurant(isCheckedRestaurant === '' ? 'Restaurant' : '')}
-                color={isCheckedRestaurant !== '' ? '#E6DCEC' : undefined}
-              />
-              <Text style={styles.checkboxText}>Favoris :</Text>
-              <Checkbox
-                style={styles.checkbox}
-                value={isCheckedFavorite}
-                onValueChange={() => setCheckedFavorite(isCheckedFavorite === '' ? 'Favoris' : '')}
-                color={isCheckedFavorite !== '' ? '#E6DCEC' : undefined}
-              />
-              <Text style={styles.checkboxText}>Promotion :</Text>
-              <Checkbox
-                style={styles.checkbox}
-                value={isCheckedPromo}
-                onValueChange={() => setCheckedPromo(isCheckedPromo === '' ? 'Promotion' : '')}
-                color={isCheckedPromo !== '' ? '#E6DCEC' : undefined}
-              />
-              <Text style={styles.checkboxText}>Evènements :</Text>
-              <Checkbox
-                style={styles.checkbox}
-                value={isCheckedEvent}
-                onValueChange={() => setCheckedEvent(isCheckedEvent === '' ? 'Evènement' : '')}
-                color={isCheckedEvent !== '' ? '#E6DCEC' : undefined}
-              />
-            </View>
+    <View style={styles.container}>
+      {/* Filter view */}
+      <View style={styles.filterContainer}>
+        <View style={styles.filter}>
+          <TextInput
+            placeholder={'Adresse, ville, métro...'}
+            style={styles.filterPlace}
+          />
+          <View style={styles.filterBottom}>
+            <TextInput
+              placeholder='Quand ?'
+              onFocus={showDatePicker}
+              value={selectedStartDate ? format(selectedStartDate, 'dd/MM/yy HH:mm') : ''}
+              style={styles.filterWhen}
+            />
+            <DateTimePickerModal
+              isVisible={isDatePickerVisible}
+              mode="datetime"
+              onConfirm={handleConfirm}
+              onCancel={() => { hideDatePicker() }}
+              locale="fr"
+              cancelTextIOS="Annuler" // A voir pour Android
+              confirmTextIOS="Confirmer" // A voir pour Android
+            />
+            <TouchableOpacity
+              style={styles.buttonFilter}
+              activeOpacity={0.8}
+              onPress={() => setIsFilterModalVisible(!isFilterModalVisible)}
+            >
+              <Text style={styles.textButtonFilter}>+ de filtres</Text>
+            </TouchableOpacity>
           </View>
         </View>
-      </TouchableWithoutFeedback>
-    </Modal>
-{/* Etablishments button */}
+      </View>
+      {/* Filter modal*/}
+      <Modal
+        animationType='fade'
+        transparent={true}
+        visible={isFilterModalVisible}
+        onRequestClose={() => setIsFilterModalVisible(false)}
+      >
+        <TouchableWithoutFeedback onPress={() => setIsFilterModalVisible(false)}>
+          <View style={styles.modal}>
+            <View style={styles.modalView}>
+              <View style={styles.checkboxContainer}>
+                <Text style={styles.checkboxText}>Bar :</Text>
+                <Checkbox
+                  style={styles.checkbox}
+                  value={isCheckedBar}
+                  onValueChange={() => setCheckedBar(isCheckedBar === '' ? 'Bar' : '')}
+                  color={isCheckedBar !== '' ? '#E6DCEC' : undefined}
+                />
+                <Text style={styles.checkboxText}>Restaurant :</Text>
+                <Checkbox
+                  style={styles.checkbox}
+                  value={isCheckedRestaurant}
+                  onValueChange={() => setCheckedRestaurant(isCheckedRestaurant === '' ? 'Restaurant' : '')}
+                  color={isCheckedRestaurant !== '' ? '#E6DCEC' : undefined}
+                />
+                <Text style={styles.checkboxText}>Favoris :</Text>
+                <Checkbox
+                  style={styles.checkbox}
+                  value={isCheckedFavorite}
+                  onValueChange={() => setCheckedFavorite(isCheckedFavorite === '' ? 'Favoris' : '')}
+                  color={isCheckedFavorite !== '' ? '#E6DCEC' : undefined}
+                />
+                <Text style={styles.checkboxText}>Promotion :</Text>
+                <Checkbox
+                  style={styles.checkbox}
+                  value={isCheckedPromo}
+                  onValueChange={() => setCheckedPromo(isCheckedPromo === '' ? 'Promotion' : '')}
+                  color={isCheckedPromo !== '' ? '#E6DCEC' : undefined}
+                />
+                <Text style={styles.checkboxText}>Evènements :</Text>
+                <Checkbox
+                  style={styles.checkbox}
+                  value={isCheckedEvent}
+                  onValueChange={() => setCheckedEvent(isCheckedEvent === '' ? 'Evènement' : '')}
+                  color={isCheckedEvent !== '' ? '#E6DCEC' : undefined}
+                />
+              </View>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+      {/* Etablishments button */}
       <View style={styles.etablishmentsContainer}>
-        <TouchableOpacity 
+        <TouchableOpacity
           style={styles.etablishments}
           activeOpacity={0.8}
           onPress={() => setEtablishmentsModalVisible(true)}
-          >
+        >
           <Text style={styles.etablishmentsList}>
             Liste des établissements
           </Text>
         </TouchableOpacity>
       </View>
-{/* Etablishments modal */}
+      {/* Etablishments modal */}
       <Modal
         animationType='fade'
         transparent={true}
         visible={EtablishmentsModalVisible}
         onRequestClose={() => setEtablishmentsModalVisible(false)}
-         >
-          <View style={styles.modal}>
-            <View style={styles.listModal}>
-              <Text 
-                style={styles.titleModal}
-                onPress={() => setEtablishmentsModalVisible(false)}
-              >Liste des établissements</Text>
-              <ScrollView style={styles.scrollList}>
-                <View>
-              
-              <View 
-                style={styles.etablishmentCard}
-                
-                >
-              <Image style={styles.image} source={require('../assets/LovsterImage.jpeg')} />
-              <TouchableOpacity onPress={() => {
-                setEtablishmentsModalVisible(false);
-                navigation.navigate('Description')}}>
-              <View style={styles.etablishmentInfo}>
-                <Text style={styles.etablishmentName}>Café Lovster</Text>
-                <Text style={styles.etablishmentDistance}>à 345 mètres</Text>
-                <Text style={styles.etablishmentNote}>Note Google: 3,9/5</Text>
-                <Text style={styles.etablishmentAdress}>3/3 Bis Boulevard Carnot, 59800 Lille</Text>
-                <Text style={styles.etablishmentPhone}>03 28 14 18</Text>
-                <Text style={styles.etablishmentEvent}>Happy hour de 18 à 20h</Text>
-                <Text style={styles.etablishmentEvent}>5 Euros la pinte</Text>
-                </View>
-                </TouchableOpacity>
-                  <View style={styles.etablishmentFavorite}>
-                    <FontAwesome name='star' color={'#D7D7E5'} size={25} />
-                  </View>
+      >
+        <View style={styles.modal}>
+          <View style={styles.listModal}>
+            <Text
+              style={styles.titleModal}
+              onPress={() => setEtablishmentsModalVisible(false)}
+            >Liste des établissements</Text>
+            <ScrollView style={styles.scrollList}>
+              <View>
+                {eventList}
               </View>
-              
-              <View style={styles.etablishmentCard}>
-              <Image style={styles.image} source={require('../assets/LovsterImage.jpeg')} />
-              <View style={styles.etablishmentInfo}>
-              <Text style={styles.etablishmentName}>Café Lovster</Text>
-                <Text style={styles.etablishmentDistance}>à 345 mètres</Text>
-                <Text style={styles.etablishmentNote}>Note Google: 3,9/5</Text>
-                <Text style={styles.etablishmentAdress}>3/3 Bis Boulevard Carnot, 59800 Lille</Text>
-                <Text style={styles.etablishmentPhone}>03 28 14 18</Text>
-                <Text style={styles.etablishmentEvent}>Happy hour de 18 à 20h</Text>
-                <Text style={styles.etablishmentEvent}>5 Euros la pinte</Text>
-                </View>
-                  <View style={styles.etablishmentFavorite}>
-                    <FontAwesome name='star' color={'#D7D7E5'} size={25} />
-                  </View>
-              </View>
-              <View style={styles.etablishmentCard}>
-              <Image style={styles.image} source={require('../assets/LovsterImage.jpeg')} />
-              <View style={styles.etablishmentInfo}>
-              <Text style={styles.etablishmentName}>Café Lovster</Text>
-                <Text style={styles.etablishmentDistance}>à 345 mètres</Text>
-                <Text style={styles.etablishmentNote}>Note Google: 3,9/5</Text>
-                <Text style={styles.etablishmentAdress}>3/3 Bis Boulevard Carnot, 59800 Lille</Text>
-                <Text style={styles.etablishmentPhone}>03 28 14 18</Text>
-                <Text style={styles.etablishmentEvent}>Happy hour de 18 à 20h</Text>
-                <Text style={styles.etablishmentEvent}>5 Euros la pinte</Text>
-                </View>
-                  <View style={styles.etablishmentFavorite}>
-                    <FontAwesome name='star' color={'#D7D7E5'} size={25} />
-                  </View>
-              </View>
-              <View style={styles.etablishmentCard}>
-              <Image style={styles.image} source={require('../assets/LovsterImage.jpeg')} />
-              <View style={styles.etablishmentInfo}>
-              <Text style={styles.etablishmentName}>Café Lovster</Text>
-                <Text style={styles.etablishmentDistance}>à 345 mètres</Text>
-                <Text style={styles.etablishmentNote}>Note Google: 3,9/5</Text>
-                <Text style={styles.etablishmentAdress}>3/3 Bis Boulevard Carnot, 59800 Lille</Text>
-                <Text style={styles.etablishmentPhone}>03 28 14 18</Text>
-                <Text style={styles.etablishmentEvent}>Happy hour de 18 à 20h</Text>
-                <Text style={styles.etablishmentEvent}>5 Euros la pinte</Text>
-                </View>
-                  <View style={styles.etablishmentFavorite}>
-                    <FontAwesome name='star' color={'#D7D7E5'} size={25} />
-                  </View>
-              </View>
-              </View>
-              </ScrollView>
-            </View>
+            </ScrollView>
           </View>
-        </Modal>
+        </View>
+      </Modal>
 
-{/* Mapview */}
+      {/* Mapview */}
       <MapView style={styles.map}
         initialRegion={{
           latitude: 50.637699,
@@ -264,6 +244,7 @@ const handleConfirm = (date) => {
           longitudeDelta: 0.0061,
         }}>
         {currentPosition && <Marker coordinate={currentPosition} title="My position" pinColor="#FF7337" />}
+        {event}
       </MapView>
     </View>
   )
@@ -284,7 +265,7 @@ const styles = StyleSheet.create({
     right: 0,
     height: 150,
     zIndex: 1, // Pour s'assurer que la view filtre reste au-dessus de la carte
-  },  
+  },
   filter: {
     backgroundColor: '#FFF',
     height: 150,
@@ -338,12 +319,12 @@ const styles = StyleSheet.create({
   },
   etablishmentsContainer: {
     position: 'absolute',
-    top: 800,
+    top: Platform.OS === 'ios' ? 800 : 650,
     left: 0,
     right: 0,
     height: 150,
     zIndex: 1, // Pour s'assurer que la view filtre reste au-dessus de la carte
-  },  
+  },
   etablishments: {
     backgroundColor: '#8440B4',
     height: 100,
@@ -461,7 +442,7 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontFamily: 'Quicksand-Regular',
   },
-  etablishmentDistance:{
+  etablishmentDistance: {
     color: '#321C3C',
     fontSize: 13,
     fontFamily: 'Quicksand-SemiBold',
@@ -485,6 +466,6 @@ const styles = StyleSheet.create({
     marginLeft: 15,
     marginTop: 10,
   }
-  
+
 
 });
